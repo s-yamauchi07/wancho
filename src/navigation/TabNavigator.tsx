1fbb33c85331a868772244d6eae88b2fa92ced7d
s-yamauchi07 <hand1nhand306@gmail.com>
2026-04-11 16:19:53 +0900

    typescriptで型の定義

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -5,16 +5,54 @@ import PlannerScreen from '@/screens/planner/PlannerScreen';
 import ReportScreen from '@/screens/report/ReportScreen';
 import SettingsScreen from '@/screens/settings/SettingsScreen';
 
-const Tab = createBottomTabNavigator();
+type TabParamList = {
+  Home: undefined;
+  Record: undefined;
+  Planner: undefined;
+  Report: undefined;
+  Settings: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator initialRouteName="ホーム">
-      <Tab.Screen name="ホーム" component={HomeScreen} />
-      <Tab.Screen name="記録" component={RecordScreen} />
-      <Tab.Screen name="プランナー" component={PlannerScreen} />
-      <Tab.Screen name="レポート" component={ReportScreen} />
-      <Tab.Screen name="設定" component={SettingsScreen} />
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen 
+        name="Home"
+        component={HomeScreen} 
+        options={{
+          title: 'ホーム',
+        }}
+      />
+      <Tab.Screen 
+        name="Record"
+        component={RecordScreen} 
+        options={{
+          title: '記録',
+        }}
+      />
+      <Tab.Screen 
+        name="Report"
+        component={ReportScreen} 
+        options={{
+          title: 'レポート',
+        }}
+      />
+      <Tab.Screen 
+        name="Planner"
+        component={PlannerScreen} 
+        options={{
+          title: 'プランナー',
+        }}
+      />
+      <Tab.Screen 
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: '設定',
+        }}
+      />
     </Tab.Navigator>
   );
 }

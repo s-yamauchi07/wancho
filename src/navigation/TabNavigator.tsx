8ab1fd784469ac9600e0cca3d8e27d1c341d3f84
s-yamauchi07 <hand1nhand306@gmail.com>
2026-04-11 15:13:49 +0900

    ナビゲーターの追加

---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,20 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '@/screens/home/HomeScreen';
+import RecordScreen from '@/screens/record/RecordScreen';
+import PlannerScreen from '@/screens/planner/PlannerScreen';
+import ReportScreen from '@/screens/report/ReportScreen';
+import SettingsScreen from '@/screens/settings/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ホーム" component={HomeScreen} />
+      <Tab.Screen name="記録" component={RecordScreen} />
+      <Tab.Screen name="プランナー" component={PlannerScreen} />
+      <Tab.Screen name="レポート" component={ReportScreen} />
+      <Tab.Screen name="設定" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/screens/report/ReportScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function ReportScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>レポート</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

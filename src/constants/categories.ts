bf815bc584e690391778c09d113e4ebfc8cfad2c
s-yamauchi07 <hand1nhand306@gmail.com>
2026-05-16 13:23:27 +0900

    chore: add categories constant with dummy data

---
+++ src/constants/categories.ts
@@ -0,0 +1,13 @@
+import { Category } from "@/types/categories";
+
+export const CATEGORIES: Category[] = [
+  { id: 1, name: 'フード' },
+  { id: 2, name: 'おやつ' },
+  { id: 3, name: '医療費' },
+  { id: 4, name: 'トリミング' },
+  { id: 5, name: 'ペット保険' },
+  { id: 6, name: 'おもちゃ' },
+  { id: 7, name: '日用品' },
+  { id: 8, name: 'ペットホテル' },
+  { id: 9, name: 'その他' },
+];
